f629da9299d1ebbf330cd33690499dc1c6ab1289
mnshoemake <themikeshoemake@gmail.com>
2019-05-19 15:58:20 -0700

    Added Homework6

---
+++ Homework6/BirthdayCardGenerator/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BirthdayCardGenerator.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult CardGenerator()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CardGenerator(Models.BirthdayCard birthdayCard)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("CardSent", birthdayCard); 
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult CardSent()
+        {
+            return View("CardGenerator");
+        }
+
+
+
+    }
+}
